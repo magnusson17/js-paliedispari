19b776e78ac98c8b8fed6f2e68abb20f069629c2
Ga <gabri.magni.97@gmail.com>
2022-03-18 14:01:14 +0100

    palidroma

--- Palidroma/asset/js/main.js
+++ Palidroma/asset/js/main.js
@@ -1,3 +1,15 @@
 /*Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma*/
 let container = document.getElementById("container");
+
+let ask = prompt("inserisci una parola");
+
+function parolaPalindroma(parola) {
+    if (parola == parola.split("").reverse().join("")) {
+        container.innerHTML = `${parola} è una parola palindroma`;
+    } else {
+        container.innerHTML = `${parola} NON è una parola palindroma`;
+    }
+}
+
+parolaPalindroma(ask);
